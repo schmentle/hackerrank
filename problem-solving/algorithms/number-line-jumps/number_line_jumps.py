6eafd86fd95a0787ebac3431f6c16b6377874ca7
Yentl Naidoo <yentl.naidoo@mtn.com>
2024-01-08 16:02:00 +0200

    Challenge: Number Line Jumps

---
+++ problem-solving/algorithms/number-line-jumps/number_line_jumps.py
@@ -0,0 +1,8 @@
+def kangaroo(x1, v1, x2, v2):
+    if v1 <= v2:
+        return "NO"
+    else:
+        if (x2 -x1) % (v1 - v2) == 0:
+            return "YES"
+        else:
+            return "NO"
